7d6adc5fe9215173cb2546550157cc3a04232969
PushRod <barfred.dk@gmail.com>
2021-04-17 10:50:30 +0200

    Tilpasset og rettet udskrift, demonstrerer output fra delegates

--- DelegateStuff/Program.cs
+++ DelegateStuff/Program.cs
@@ -3,19 +3,22 @@
 namespace DelegateStuff
 {
     delegate double OperatorDelegate(double x, double y);
+    delegate int opx (double x, double y);
 
     class Arithmetic
-    {
-        internal double Plus(double m, double n) { Console.WriteLine("Plus: " + (m + n)); return m + n; }
-        internal double Gange(double m, double n) { Console.WriteLine("Mul: " + (m * n)); return m * n; }
-        internal double Minus(double m, double n) { Console.WriteLine("Sub: " + (m - n)); return m - n; }
-        internal double Dele(double m, double n) { Console.WriteLine("Div: " + (m / n)); return m / n; }
+    {   //Har sat metoderne på en linje, præcis det samme 
+        internal double Plus(double m, double n)  {Console.WriteLine($"Addition:       {m} + {n} = {m + n}"); return m + n; }
+        internal double Gange(double m, double n) {Console.WriteLine($"Multiplikation: {m} x {n} = {m * n}"); return m * n; }
+        internal double Minus(double m, double n) {Console.WriteLine($"Subtraction:    {m} - {n} = {m - n}"); return m - n; }
+        internal double Dele(double m, double n)  {Console.WriteLine($"Division:       {m} / {n} = {m / n}"); return m / n; }
     }
-
+        
     class Program
     {
         static void Main()
         {
+            //Har soigneret outputtet 
+
             Arithmetic m = new Arithmetic();
 
             // Delegate instantiation.
@@ -28,21 +31,30 @@ namespace DelegateStuff
             mathOpr(16, 2);  //Multicast -alle 4 regningsarter. Output -> 18, 32, 14, 8
             mathOpr.Invoke(8, 2); //Ingen forskel ift ovenst., bortset fra output --> 10, 16, 6, 4
 
+            Console.WriteLine("Brug af foreach:");
+            double nonsenseTotal = 0; //akkumulerer alle resultaterne fra de 4 regningsarter. Hvorfor: for at demonstrere outputtet fra delegate item
             foreach (Delegate item in mathOpr.GetInvocationList())
             {
                 //Looper alle delegates i multicast igennem og eksekverer dem
-                Console.WriteLine(item.DynamicInvoke(20, 2) + ": " + item.Method.Name);
+                nonsenseTotal += (double)item.DynamicInvoke(20, 2);
             }
-            
-            //Filtrer: kun addition og multiplikation må eksekveres
+            Console.WriteLine("Nonsense total på alle: " + nonsenseTotal);
+            //filtrer: kun addition og multiplikation må eksekveres
+
+            Console.WriteLine("Filtreret på Plus og Gange:");
+            nonsenseTotal = 0;
+            double arg1 = 20, arg2 = 5;
             foreach (Delegate item in mathOpr.GetInvocationList())
             {
+                //Filtrer: kun addition og multiplikation må eksekveres
                 if (item.Method.Name == "Plus")
-                    Console.WriteLine(item.DynamicInvoke(20, 5) + ": " + item.Method.Name);
+                   nonsenseTotal += (double) item.DynamicInvoke(arg1, arg2);
+                
                 if (item.Method.Name == "Gange")
-                    Console.WriteLine(item.DynamicInvoke(20, 5) + ": " + item.Method.Name);
+                    nonsenseTotal += (double)item.DynamicInvoke(arg1, arg2);  //redundant, burde refaktoreres
+
             }
-            
+            Console.WriteLine("Nonsense total på plus og gange: " + nonsenseTotal);
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
